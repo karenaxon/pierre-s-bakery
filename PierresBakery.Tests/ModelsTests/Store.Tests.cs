9e0911bd739f2b58a79387eb34a0f6a01addbbe8
Kaxon <karenaxon@users.noreply.github.com>
2021-12-10 09:18:26 -0800

    Green test for Bread class object.

---
+++ PierresBakery.Tests/ModelsTests/Store.Tests.cs
@@ -0,0 +1,19 @@
+using System.Reflection.Metadata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class StoreTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread bread = new Bread();
+      Assert.AreEqual(typeof(Bread), bread.GetType());
+    }
+
+  }
+}
---
+++ PierresBakery/Models/Bread.cs
@@ -0,0 +1,8 @@
+namespace PierresBakery.Models
+// make sure the namespace matches the name of your project (the equivalent of ProjectName)
+{
+  public class Bread
+  {
+    
+  }
+}
